5978e5a7cda97c549de2ae7997a96aef72435f0c
jiang <jing>
2020-09-18 17:15:56 +0800

    feat: KemButton组件增加下拉按钮组功能

--- packages/Button/src/main.vue
+++ packages/Button/src/main.vue
@@ -1,126 +1,168 @@
 <script lang="jsx">
 import { throttle ,get} from 'lodash'
 export default {
-  name: 'KemButton',
-  components: {},
-  props: {
-    type: {
-      type: String,
-      default: 'primary'
+    name: 'KemButton',
+    components: {},
+    props: {
+        type: {
+            type: String,
+            default: 'primary'
+        },
+        size: {
+            type: String,
+            default: 'small'
+        },
+        // 下拉按钮组默认不显示
+        selectButtonGroup: {
+            type: [Array,Boolean],
+            default: false
+        },
     },
-    size: {
-      type: String,
-      default: 'small'
-    },
-  },
-  computed:{
-    evet(){
-      if (this.$listeners.click) {
-        console.log(this.$attrs)
-        // eslint-disable-next-line vue/no-side-effects-in-computed-properties
-        this.$listeners.click = this.throttle("click");
-      }
-      return this.$listeners;
-    },
-    style(){
-      if(this.$props.type === 'text'){
+    computed:{
+        evet(){
+            if (this.$listeners.click) {
+                console.log(this.$attrs)
+                // eslint-disable-next-line vue/no-side-effects-in-computed-properties
+                this.$listeners.click = this.throttle("click");
+            }
+            return this.$listeners;
+        },
+        style(){
+            if(this.$props.type === 'text'){
 
-        return{
-          "padding-top":"0px",
-          "padding-bottom": "0px"
+                return{
+                    "padding-top":"0px",
+                    "padding-bottom": "0px"
+                }
+            }return {}
+
         }
-      }return {}
-
-    }
-  },
-  created(){
-    if(this.$listeners.click){
-      this.$listeners.click = () =>{}
-
-    }
-  },
-
-  render(){
-    const { type } = this
-
-    const baseButton = ()=>{
-      return(
-              <el-button
-                      props={this.$attrs}
-                      type={this.type}
-                      size={this.size}
-                      style={this.style}
-                      on={this.evet}
-              >
-                {this.$slots.default}
-              </el-button>
-      )
-    }
-
-    const specialButton = (type)=>{
-
-
-
-      const specialButtonConfig = {
-        operate:{
-          del:{
-            type:'danger',
-            text:'删除',
-            icon:"el-icon-delete"
-          },
-          edit:{
-            type:'primary',
-            text:'编辑',
-            icon:"el-icon-edit"
-          },
-          add:{
-            type:'success',
-            text:'新增',
-            icon:"el-icon-plus"
-          },
-        }
-      }
-
-
-
-      return(
-              <el-button
-                      size="mini"
-                      type={get(specialButtonConfig,type).type}
-                      icon={get(specialButtonConfig,type).icon}
-                      on={this.evet}
-                      round={true}>
-                {this.$slots.default || get(specialButtonConfig,type).text}
-              </el-button>
-
-
-      )
-    }
-    return (type.indexOf('.')!==-1?specialButton(type):baseButton(type))
-  },
-
-  methods: {
-    /**
-     * 节流
-     * @param method
-     */
-    throttle(method){
-      return throttle(
-              (...args)=>{
-                this.$emit(method, ...args);
-              }, this.$attrs.wait || 1000, { //[wait=0] (number): 需要节流的毫秒数。
-                // [options.leading=true] (boolean): 指定调用在节流开始前，默认true。
-                // [options.trailing=true] (boolean): 指定调用在节流结束后，默认true。
-                leading: true,
-                trailing: false
-              }
-      )
     },
-  }
+    created(){
+        if(this.$listeners.click){
+            this.$listeners.click = () =>{}
+
+        }
+    },
+
+    methods: {
+        /**
+         * 节流
+         * @param method
+         */
+        throttle(method){
+            return throttle(
+                (...args)=>{
+                    this.$emit(method, ...args);
+                }, this.$attrs.wait || 1000, { //[wait=0] (number): 需要节流的毫秒数。
+                    // [options.leading=true] (boolean): 指定调用在节流开始前，默认true。
+                    // [options.trailing=true] (boolean): 指定调用在节流结束后，默认true。
+                    leading: true,
+                    trailing: false
+                }
+            )
+        },
+    },
+
+    render(){
+        const { type } = this
+
+        const baseButton = ()=>{
+            return(
+                <el-button
+                    props={this.$attrs}
+                    type={this.type}
+                    size={this.size}
+                    style={this.style}
+                    on={this.evet}
+                >
+                    {this.$slots.default}
+                </el-button>
+            )
+        }
+
+        const selectButtonGroup = (button)=>{
+
+            const options = this.selectButtonGroup
+
+
+            const dropdownItem = () =>{
+                let eles = []
+                options.map(item=>{
+                    eles.push( <el-dropdown-item command={item.value}>{item.label}</el-dropdown-item>)
+                })
+                return eles
+            }
+
+            const handleCommand = (value) =>{
+
+                this.$emit('clickButtonItem',{value})
+
+            }
+
+
+
+            return(
+                <el-dropdown  v-on:command={handleCommand}>
+                    <div style="padding: 0 8px">
+                        { button }
+                    </div>
+                    <el-dropdown-menu slot="dropdown" >
+                        {dropdownItem}
+                    </el-dropdown-menu>
+                </el-dropdown>
+            )
+        }
+
+        const specialButton = (type)=>{
+
+
+
+            const specialButtonConfig = {
+                operate:{
+                    del:{
+                        type:'danger',
+                        text:'删除',
+                        icon:"el-icon-delete"
+                    },
+                    edit:{
+                        type:'primary',
+                        text:'编辑',
+                        icon:"el-icon-edit"
+                    },
+                    add:{
+                        type:'success',
+                        text:'新增',
+                        icon:"el-icon-plus"
+                    },
+                }
+            }
+
+
+
+            return(
+                <el-button
+                    size="mini"
+                    type={get(specialButtonConfig,type).type}
+                    icon={get(specialButtonConfig,type).icon}
+                    on={this.evet}
+                    round={true}>
+                    {this.$slots.default || get(specialButtonConfig,type).text}
+                </el-button>
+
+
+            )
+        }
+        const btn = type.indexOf('.')!==-1?specialButton(type):baseButton(type)
+        if(this.selectButtonGroup){
+            return (selectButtonGroup(btn))
+        }else {
+            return btn
+        }
+    }
 }
 </script>
-<style lang="scss">
-</style>
+
 
 
 
--- src/views/Button.vue
+++ src/views/Button.vue
@@ -1,5 +1,6 @@
 <template>
   <div class="about">
+    <KemButton :select-button-group="selectButtonGroup" @clickButtonItem='clickButtonItem' type="operate.del" />
     <KemButton type="operate.del" />
     <KemButton type="operate.del">
       删除你个鬼东西
@@ -12,5 +13,24 @@
     </KemButton>
   </div>
 </template>
+<script >
+export default {
+  data() {
+    return {
+      selectButtonGroup: [
+        { value:'11', label:'11'},
+        { value:'2', label:'2'},
+        { value:'4', label:'3'},
+      ]
+    }
+  },
+  methods: {
+    clickButtonItem({value}) {
+      console.log(value)
+
+    }
+  },
+}
+</script>
 
 
