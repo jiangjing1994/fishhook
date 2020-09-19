25ae54c33de2aba2bc4f45955928d7df59760fa6
jiang <jing>
2020-09-19 18:07:57 +0800

    feat: 增加KemSvgIcon组件

--- packages/SvgIcon/src/main.vue
+++ packages/SvgIcon/src/main.vue
@@ -1,15 +1,15 @@
 <template>
   <div
-    v-if="isExternal"
-    :style="styleExternalIcon"
-    class="svg-external-icon svg-icon"
-    v-on="$listeners"
+          v-if="isExternal"
+          :style="styleExternalIcon"
+          class="svg-external-icon svg-icon"
+          v-on="$listeners"
   />
   <svg
-    v-else
-    :class="svgClass"
-    aria-hidden="true"
-    v-on="$listeners"
+          v-else
+          :class="svgClass"
+          aria-hidden="true"
+          v-on="$listeners"
   >
     <use :xlink:href="iconName" />
   </svg>
