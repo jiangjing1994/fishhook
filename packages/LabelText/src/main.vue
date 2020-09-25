2472a383925fa751810fc1027a6b716244682a17
yangyh <283291662@qq.com>
2020-09-25 12:17:56 +0800

    feate: KemLabelText组件

---
+++ packages/LabelText/src/main.vue
@@ -0,0 +1,27 @@
+<template lang="pug">
+    span {{value}}
+</template>
+<script>
+export default {
+    name: 'KemLabelText',
+
+    props: {
+        value: {
+            type: String,
+            default: ''
+        }
+    },
+    watch: {
+        value(newValue) {
+            this.$emit('input', newValue)
+        }
+    },
+
+    methods: {
+    },
+}
+</script>
+
+<style scoped>
+
+</style>
