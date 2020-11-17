508d7cf285bf1d0eb41e8236ee48e41b2c1f5b53
jiang <jing>
2020-11-17 14:26:44 +0800

    feat: 增加颜色选择器

---
+++ packages/ColorPicker/src/main.vue
@@ -0,0 +1,19 @@
+<template lang="pug">
+    el-color-picker( v-bind="$attrs" v-on="evet" )
+</template>
+<script>
+export default {
+    name: 'KemColorPicker',
+    props: {
+        placeholder: {
+            type: String,
+            default: '请输入'
+        },
+    },
+    computed:{
+        evet(){
+            return this.$listeners;
+        },
+    },
+}
+</script>
