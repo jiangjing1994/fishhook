6148225039020894d25cc54c97b5210e269f3d4a
jiang <jing>
2021-01-26 20:32:53 +0800

    feat: 修改steps

--- src/views/demo/Steps.vue
+++ src/views/demo/Steps.vue
@@ -1,14 +1,7 @@
 <template>
     <div style="text-align: left">
         <KemSteps :title-list="titleList" :active="active"></KemSteps>
-        <el-steps direction="vertical" :space="80" :active="active" finish-status="success">
-            <el-step title="步骤 1" :description="active < 1 ? '正在进行步骤一' : '步骤一已完成'"></el-step>
-            <el-step title="步骤 2" :description="active < 2 ? '正在进行步骤二' : '步骤二已完成'"></el-step>
-            <el-step title="步骤 3" :description="active < 3 ? '正在进行步骤三' : '步骤三已完成'"></el-step>
-            <el-step title="步骤 4" :description="active < 4 ? '正在进行步骤四' : '步骤四已完成'"></el-step>
-            <el-step title="步骤 5" :description="active < 5 ? '正在进行步骤五' : '步骤五已完成'"></el-step>
-            <el-step title="步骤 6" :description="active < 6 ? '正在进行步骤六' : '步骤六已完成'"></el-step>
-        </el-steps>
+
     </div>
 </template>
 
