2da7f300f089c164619623c272ea84f8d242b346
jiang <jing>
2021-04-20 17:53:51 +0800

    test

---
+++ build/gen-version.js
@@ -0,0 +1,16 @@
+const fs = require('fs')
+const path = require('path')
+const tagVer = process.env.TAG_VERSION
+let version
+if (tagVer) {
+  version = tagVer.startsWith('v') ? tagVer.slice(1) : tagVer
+} else {
+  version = require('../package.json').version
+}
+console.log(version)
+
+fs.writeFileSync(
+  path.resolve(__dirname, '../packages/element-plus/version.ts'),
+  `export const version = '${version}'
+`
+)
