d2f1f7cf587dd428a8f4c9c2926b06fc282bd174
jiang <jing>
2021-05-25 15:55:41 +0800

    add file

---
+++ packages/Icon/src/main.vue
@@ -0,0 +1,58 @@
+<template>
+  <i :class="classes" :style="styles" @click="handleClick"></i>
+</template>
+
+<script>
+/**
+ * @displayName KemIcon
+ */
+const prefixCls = 'ivu-icon';
+export default {
+  name: 'KemIcon',
+  props: {
+    type: {
+      type: String,
+      default: ''
+    },
+    size: [Number, String],
+    color: String,
+    custom: {
+      type: String,
+      default: ''
+    }
+  },
+  computed: {
+    classes () {
+      return [
+        `${prefixCls}`,
+        {
+          [`${prefixCls}-${this.type}`]: this.type !== '',
+          [`${this.custom}`]: this.custom !== '',
+        }
+      ];
+    },
+    styles () {
+      let style = {};
+
+      if (this.size) {
+        style['font-size'] = `${this.size}px`;
+      }
+
+      if (this.color) {
+        style.color = this.color;
+      }
+
+      return style;
+    }
+  },
+  methods: {
+    handleClick (event) {
+      this.$emit('click', event);
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+
+</style>
---
+++ src/views/demo/Icon.vue
@@ -0,0 +1,46 @@
+
+<template>
+  <div style="text-align: left">
+    <KemTree :data="baseData" show-checkbox></KemTree>
+    <KemViewTree :data="baseData" show-checkbox></KemViewTree>
+
+  </div>
+ </template>
+
+
+ <script>
+ export default {
+   name: 'KemIcon',
+
+   data () {
+     return {
+       baseData: [{
+         expand: true,
+         title: 'parent 1',
+         children: [{
+           title: 'parent 1-0',
+           expand: true,
+           disabled: true,
+           children: [{
+             title: 'leaf',
+             disableCheckbox: true
+           }, {
+             title: 'leaf',
+           }]
+         }, {
+           title: 'parent 1-1',
+           expand: true,
+           checked: true,
+           children: [{
+             title: '<span style="color: red">leaf</span>',
+           }]
+         }]
+       }]
+     }
+   }
+ }
+ </script>
+
+ <style lang="scss" scoped>
+
+</style>
