2c761e3afe02f5caea8c0b74c823973d808de98e
JiangJing <jiangjing1@yusys.com.cn>
2021-09-03 11:21:59 +0800

    feat(form): 完善栅格布局

--- examples/views/demo/Form.vue
+++ examples/views/demo/Form.vue
@@ -95,7 +95,7 @@ export default {
         ],
       },
       formItems: [
-        { label: '活动名称', prop: 'name', span: 8, tip: 'sasa' },
+        { label: '活动名称', prop: 'name', span: 8,  offset: 16, pull: 16, tip: 'sasa' },
         { label: '电子邮箱', prop: 'email', span: 8 },
         { label: '人员总数', prop: 'num', span: 8, component: 'KemInputNumber' },
         {
