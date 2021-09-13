691e5c23160a8cf33eff2e6ccd98502b2fa143e1
JiangJing <jiangjing1@yusys.com.cn>
2021-09-13 10:31:29 +0800

    feat(d52tButton): 增加特殊按钮组件

--- examples/views/demo/D52tButton.vue
+++ examples/views/demo/D52tButton.vue
@@ -1,13 +1,28 @@
 <template>
   <div>
-    <KemD52tButton></KemD52tButton>
+    <KemD52tButton value="123" @focus="focus"
+                   @click="click"
+></KemD52tButton>
+    <KemD52tButton value="123" @focus="focus"
+                   @click="click"
+>111</KemD52tButton>
   </div>
 
 </template>
 
 <script>
 export default {
-  name: "D52tButton"
+  name: "D52tButton",
+  methods: {
+    focus() {
+
+      console.log(111111)
+    },
+    click() {
+      console.log(22222222222)
+
+    },
+  },
 }
 </script>
 
--- packages/D52tButton/src/main.vue
+++ packages/D52tButton/src/main.vue
@@ -1,35 +1,45 @@
- <template>
-    <div>
-      <KemInput readonly/>
+<template>
+  <div @click="focus">
+    <KemInput v-model="value" readonly @click.stop="focus">
+      <span slot="suffix">
+          <i v-if="!$slots.default" class="el-input__icon el-icon-circle-close kem-d52t-button-icon" @click.stop="click"></i>
+        <span v-else @click.stop="click">
+                   <slot></slot>
 
-    </div>
+        </span>
+       </span>
+
+    </KemInput>
+  </div>
 </template>
 
- <script>
- /**
-  * @displayName KemD52tButton
-  */
+<script>
+/**
+ * @displayName KemD52tButton
+ */
 export default {
-    name: 'KemD52tButton',
-    props: {
-        propName: {
-            type: Number,
-            default: 0
-        },
+  name: 'KemD52tButton',
+  props: ['value'],
+  data() {
+    return {}
+  },
+  methods: {
+    focus() {
+       this.$emit('focus')
     },
-    data() {
-        return {
-            value: ''
-        }
-    },
-    methods: {
-        name() {
+    click() {
+      this.$emit('click')
 
-        }
     },
+  },
 }
 </script>
 
- <style lang="scss" scoped>
-
+<style lang="scss"  >
+.kem-d52t-button-icon{
+  cursor: pointer;
+  &:hover{
+    color: #0f80ff;
+  }
+}
 </style>
