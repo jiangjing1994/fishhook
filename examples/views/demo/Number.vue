1fe7db94f98090287e132c487618cbce02f359be
JiangJing <jiangjing1@yusys.com.cn>
2021-09-24 13:52:43 +0800

    feat(input-number): 增加uitype

---
+++ examples/views/demo/Number.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+    <KemInputNumber v-model="v"></KemInputNumber>
+    <KemInputNumber v-model="v" ui-type="text"></KemInputNumber>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Number',
+  data() {
+    return {
+      v: 123,
+    }
+  },
+}
+</script>
+
+<style scoped></style>
